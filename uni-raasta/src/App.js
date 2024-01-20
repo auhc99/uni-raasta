76335dccae7f1de1accef90064cbebd2fd9fc665
auhc99 <auhc99@gmail.com>
2024-01-20 12:53:54 +0800

    Base commit

---
+++ uni-raasta/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      {/* Other components can go here */}
+    </div>
+  );
+}
+
+export default App;
